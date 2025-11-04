39bed21ae0acc83d7254ea9592077c9cc4c82e4c
Navid Farahzadi <navid.farahzadi@zoodfood.com>
2025-11-04 16:57:46 +0330

    first version

--- content.js
+++ content.js
@@ -1,8 +1,11 @@
 function log(...args) {
-    console.log('[foodong]: ', ...args);
+    console.log('[foodong] ', ...args);
 }
 
 const bottomSheetSelector = '.byn-bottom-sheet__content';
+
+
+const calcDiscountedPrice = (totalPrice, price, discount) => { return Math.round(Number(price) * Number(discount) / Number(totalPrice)) }
 
 const observer = new MutationObserver((mutations) => {
     for (const m of mutations) {
@@ -16,20 +19,45 @@ const observer = new MutationObserver((mutations) => {
                 el.style.border = "1px solid orange";
                 const rowElement = el.querySelectorAll('.byn-row--narrower');
                 let items = [];
+                let discount = null;
                 if (!rowElement.length) return;
                 rowElement.forEach(elem => {
                     if (!elem) return;
                     const desc = elem.querySelector('.byn-row-content__desc')?.textContent;
-                    const title = elem.querySelector('.byn-row-content__title')?.textContent;
                     const price = elem.querySelector('.price')?.textContent;
-                    const discount = elem.querySelector('.khewPs')?.textContent;
-                    if (price && desc && !discount) {
-                        items.push({ name: desc, value: price })
+                    const priceNumber = parseInt(price) * 1000;
+                    const pElements = Array.from(elem.querySelectorAll('.byn-row__left-element p')).map(elem => elem.textContent);
+                    const multiplier = parseInt(pElements.find((elem) => /^\d+×$/.test(elem)));
+                    const discountElement = elem.querySelector('.khewPs');
+                    if (price && desc && !discountElement) {
+                        items.push({ name: desc, value: priceNumber, multiplier: multiplier ? multiplier : 1 });
                     }
-                    if (title)
-                        log(title, price);
+                    if (discountElement) {
+                        discount = priceNumber;
+                    }
+
                 });
-                log(items);
+                const totalPrice = items.reduce((acc, elem) => elem.value * elem.multiplier + acc, 0);
+                const itemsWithDiscount = items.map(elem => {
+                    elem.discount = calcDiscountedPrice(totalPrice, elem.value, discount);
+                    return elem;
+                })
+                // log(itemsWithDiscount);
+                // log('total price: ', totalPrice);
+                const priceRows = Array.from(rowElement).filter(elem => {
+                    const price = elem.querySelector('.price')?.textContent;
+                    const desc = elem.querySelector('.byn-row-content__desc')?.textContent;
+                    const discountElement = elem.querySelector('.khewPs');
+                    return price && desc && !discountElement;
+                })
+                priceRows.forEach((elem, i) => {
+                    const resultElement = document.createElement('div');
+                    const { value, discount } = itemsWithDiscount[i];
+                    const result = Number(value - discount);
+                    resultElement.classList.add('__result__')
+                    resultElement.innerHTML = result.toLocaleString(('fa-ir')) + ' تومان';
+                    elem.querySelector('.price').prepend(resultElement);
+                })
             }
         }
     }
